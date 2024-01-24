8b714fa3ebafbfba843c6f32bc378fcd7dd06288
markup54 <marzenakupka@tlen.pl>
2024-01-24 16:49:56 +0100

    otwieranie pliku

--- WpfAppNotatnik3Psroda/MainWindow.xaml.cs
+++ WpfAppNotatnik3Psroda/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,6 +40,35 @@ namespace WpfAppNotatnik3Psroda
         private void MenuItem_Unchecked(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if(tekst.Text != "")
+            {
+                var Rezultat =MessageBox.Show("Czy na pewno chcesz usunąć zawartość", "Pytanie",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if(Rezultat == MessageBoxResult.Yes)
+                {
+                    tekst.Text = string.Empty;
+                }
+                
+            }
+            
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            var oknoDialogowe = new OpenFileDialog();
+            //oknoDialogowe.FileName = "Dokument";
+            oknoDialogowe.DefaultExt = ".txt";
+            oknoDialogowe.Filter = "PlainText |*.txt";
+            bool? rezultat = oknoDialogowe.ShowDialog();
+            if(rezultat == true)
+            {
+                string nazwaPliku = oknoDialogowe.FileName;
+                tekst.Text = File.ReadAllText(nazwaPliku);
+            }
         }
     }
 }
