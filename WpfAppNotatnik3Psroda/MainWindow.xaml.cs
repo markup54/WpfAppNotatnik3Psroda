a8bd63454fa8d79408d381f48335edad501bd3f6
markup54 <marzenakupka@tlen.pl>
2024-01-24 16:15:15 +0100

    Dodaj pliki projektów.

---
+++ WpfAppNotatnik3Psroda/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppNotatnik3Psroda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_Autor(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Autorem jest klasa 3P", "O autorze");
+        }
+
+        private void MenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
